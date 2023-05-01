d3282f03f955d379120f3daf4abcf99dc8ff9bd1
itshmnt <hemantxs5@gmail.com>
2023-05-01 15:02:10 +0530

    Starting firebase server

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+require("dotenv").config();
+
+const serviceAccountKey = require("./serviceAccountKey.json");
+
+const express = require("express");
+const app = express();
+
+// Body parser for json data
+app.use(express.json());
+
+// cross origin
+const cors = require("cors");
+app.use(cors({ origin: true }));
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+// firebase credentials
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
+
+// api endpoints
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
+
+exports.app = functions.https.onRequest(app);
